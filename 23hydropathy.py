295f3e121b582afc945402bb5b6fd8e495d08bdc
Devin Fan <devinfan@MacBook-Air-359.local>
2024-01-19 13:55:28 -0800

    new

---
+++ 23hydropathy.py
@@ -0,0 +1,70 @@
+def hydro (x):
+
+	if x == "A":
+		print("1.8")
+		
+	elif x == "R":
+		print("-4.5")
+
+	elif x == "N":
+		print("-3.5")
+		
+	elif x == "D":
+		print("-3.5")
+		
+	elif x == "C":
+		print("2.5")
+		
+	elif x == "Q":
+		print("-3.5")
+		
+	elif x == "E":
+		print("-3.5")
+		
+	elif x == "G":
+		print(-0.4)
+		
+	elif x == "H":
+		print("-3.2")
+		
+	elif x == "I":
+		print("4.5")
+		
+	elif x == "L":
+		print("3.8")
+		
+	elif x == "K":
+		print("-3.9")
+		
+	elif x == "M":
+		print("1.9")
+		
+	elif x == "F":
+		print(2.8)
+		
+	elif x == "P":
+		print("-1.6")
+		
+	elif x == "S":
+		print("-0.8")
+		
+	elif x == "T":
+		print("0.7")
+		
+	elif x == "W":
+		print("-0.9")
+		
+	elif x == "V":
+		print("4.2")
+		
+	elif x == "Y":
+		print("-1.3")
+		
+	else:
+		print("Error: not amino acid or typo")
+		
+hydro("A")
+hydro("K")
+hydro("Y")
+hydro("M")
+hydro("R")
