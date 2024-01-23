c3e60bfa7ce1563ed5b4b1b07bd6e64fd19008d0
Devin Fan <devinfan@MacBook-Air-359.local>
2024-01-23 09:29:15 -0800

    new

--- quiz1.sh
+++ quiz1.sh
@@ -1,6 +1,9 @@
+#Co-Authors: Devin Fan, Sophia Chen
 echo "Real Name: Devin Fan"
 echo "Linux Username: $USER"
-gunzip -c dictionary.gz | grep "r" | grep -E "[moucfta]{4,15}" | grep -v "[^moucfta]" | wc
-gunzip -c dictionary.gz | grep "r" | grep -E "[tailnrb]{4,15}" | grep -v "[^tailnrb]" | wc
-gunzip -c dictionary.gz | grep "r" | grep -E "[maonidc]{4,15}" | grep -v "[^maonidc]" | wc
-gunzip -c dictionary.gz | grep "r" | grep -E "[anorgiz]{4,15}" | grep -v "[^anorgiz]" | wc
+gunzip -c dictionary.gz | grep "a" | grep -E "[moucfta]{4,}" | grep -v "[^moucfta]" | wc
+gunzip -c dictionary.gz | grep "b" | grep -E "[tailnrb]{4,}" | grep -v "[^tailnrb]" | wc
+gunzip -c dictionary.gz | grep "c" | grep -E "[maonidc]{4,}" | grep -v "[^maonidc]" | wc
+gunzip -c dictionary.gz | grep "z" | grep -E "[anorgiz]{4,}" | grep -v "[^anorgiz]" | wc
+
+gunzip -c jaspar2024_core.transfac.gz
