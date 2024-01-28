1399299aa85ba14a8a182ca534fca0f1094bb510
Devin Fan <devinfan@MacBook-Air-359.local>
2024-01-27 17:46:05 -0800

    new

--- 21quadratic.py
+++ 21quadratic.py
@@ -3,8 +3,8 @@ import math
 
 def quad (a, b, c):
 	
-	x1 = -b + math.sqrt(b**2 - 4 * a * c)
-	x2 = -b - math.sqrt(b**2 - 4 * a * c)
+	x1 = (-b + math.sqrt(b**2 - 4 * a * c)) / 2 * a
+	x2 = (-b - math.sqrt(b**2 - 4 * a * c)) / 2 * a
 	
 	return x1, x2
 
