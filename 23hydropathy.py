4080b927786c323fbed2c6583bdf1d82722cdafc
Devin Fan <devinfan@campus-002-147.ucdavis.edu>
2024-01-30 14:23:57 -0800

    new

--- 23hydropathy.py
+++ 23hydropathy.py
@@ -1,72 +1,49 @@
-#Co-Authors - Devin Fan, Sophia Chen
-def hydro(x):
+#23hydropathy.py by Sophia Chen & Devin Fan
 
-	if x == "A":
-		print("1.8")
-		
-	elif x == "R":
-		print("-4.5")
+import sys
 
-	elif x == "N":
-		print("-3.5")
-		
-	elif x == "D":
-		print("-3.5")
-		
-	elif x == "C":
-		print("2.5")
-		
-	elif x == "Q":
-		print("-3.5")
-		
-	elif x == "E":
-		print("-3.5")
-		
-	elif x == "G":
-		print(-0.4)
-		
-	elif x == "H":
-		print("-3.2")
-		
-	elif x == "I":
-		print("4.5")
-		
-	elif x == "L":
-		print("3.8")
-		
-	elif x == "K":
-		print("-3.9")
-		
-	elif x == "M":
-		print("1.9")
-		
-	elif x == "F":
-		print(2.8)
-		
-	elif x == "P":
-		print("-1.6")
-		
-	elif x == "S":
-		print("-0.8")
-		
-	elif x == "T":
-		print("0.7")
-		
-	elif x == "W":
-		print("-0.9")
-		
-	elif x == "V":
-		print("4.2")
-		
-	elif x == "Y":
-		print("-1.3")
-		
+def kyte(aa):
+	if aa == 'A':
+		hydro_value = 1.80
+	elif aa == 'C':
+		hydro_value = 2.50
+	elif aa == 'D' or aa == 'E' or aa == 'N' or aa == 'Q':
+		hydro_value = -3.50
+	elif aa == 'F':
+		hydro_value = 2.80
+	elif aa == 'G':
+		hydro_value = -0.40
+	elif aa == 'H':
+		hydro_value = -3.20
+	elif aa == 'I':
+		hydro_value = 4.50
+	elif aa == 'K':
+		hydro_value = -3.90
+	elif aa == 'L':
+		hydro_value = 3.80
+	elif aa == 'M':
+		hydro_value = 1.90
+	elif aa == 'P':
+		hydro_value = -1.60
+	elif aa == 'R':
+		hydro_value = -4.50
+	elif aa == 'S':
+		hydro_value = -0.80
+	elif aa == 'T':
+		hydro_value = -0.70
+	elif aa == 'V':
+		hydro_value = 4.20
+	elif aa == 'W':
+		hydro_value = -0.90
+	elif aa == 'Y':
+		hydro_value = -1.30
 	else:
-		print("Error: not amino acid or typo")
+		sys.exit('Error: not amino acid or typo')
+	print(hydro_value)
+	return hydro_value
 		
-hydro("A")
-hydro("K")
-hydro("Y")
-hydro("M")
-hydro("R")
-hydro("O")
+kyte('A')
+kyte('C')
+kyte('Q')
+kyte('M')
+kyte('O')
