8db04cc8150d94767b25600aa2d889094266d41d
Devin Fan <devinfan@MacBook-Air-359.local>
2024-02-03 10:41:28 -0800

    new

--- 34scoringmatrix.py
+++ 34scoringmatrix.py
@@ -1,12 +1,16 @@
-#Co-Authors 
-header_line = "   A  C  G  T"
-print(header_line)
+#Co-Authors - Devin Fan, Sophia Chen
 alph = 'ACGT'
+
+print(" ", end=' ')
+for i in alph:
+	print(" " + i, end=' ')
+print()
+
 for nt1 in alph:
 	print(nt1, end=' ')
 	for nt2 in alph:
 		if nt1 == nt2:
-			print('+1', end= ' ')
-		else: 
-			print('-1', end= ' ')
+			print('+1', end=' ')
+		else:
+			print('-1', end=' ')
 	print()
