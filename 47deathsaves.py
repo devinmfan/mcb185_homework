f67e8cdc0228a780641fd1a0277da4f596fb36ad
Devin Fan <devinfan@MacBook-Air-359.local>
2024-02-09 15:26:12 -0800

    new

--- 47deathsaves.py
+++ 47deathsaves.py
@@ -3,13 +3,12 @@
 import random
 
 fail = 0
-suc = 0
+rev = 0
 stab = 0
 
 for i in range(10000):
 	failures = 0
 	successes = 0
-	health = 0
 	while failures < 3 and successes < 3:
 		roll = random.randint(1, 20)
 
@@ -19,20 +18,18 @@ for i in range(10000):
 			failures += 1
 		elif roll >= 10 and roll < 20:
 			successes += 1
-		else:
-			health += 1
 	
 	if successes == 3:
-		suc = suc + 1
+		stab = stab + 1
 	elif failures == 3:
 		fail = fail + 1
 	else:
-		stab = stab + 1
+		rev = rev + 1
 
-suc = suc / 10000
-fail = fail / 10000
 stab = stab / 10000
+fail = fail / 10000
+rev = rev / 10000
 
-print("Probabilty of being revived:", suc)
+print("Probabilty of being revived:", rev)
 print("Probabilty of being stabailized:", stab)
 print("Probablity of dying:", fail)
