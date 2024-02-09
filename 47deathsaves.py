fd11560b6295c721a95a47bb07a0d928ba31a692
Devin Fan <devinfan@MacBook-Air-359.local>
2024-02-09 15:39:38 -0800

    new

--- 47deathsaves.py
+++ 47deathsaves.py
@@ -9,21 +9,24 @@ stab = 0
 for i in range(10000):
 	failures = 0
 	successes = 0
-	while failures < 3 and successes < 3:
+	revives = 0
+	while failures < 3 and successes < 3 and revives < 1:
 		roll = random.randint(1, 20)
 
 		if roll == 1:
 			failures += 2
 		elif roll < 10:
 			failures += 1
-		elif roll >= 10 and roll < 20:
+		elif roll == 20:
+			revives += 1
+		elif roll >= 10: 
 			successes += 1
 	
 	if successes == 3:
 		stab = stab + 1
-	elif failures == 3:
+	elif failures >= 3:
 		fail = fail + 1
-	else:
+	elif revives == 1:
 		rev = rev + 1
 
 stab = stab / 10000
