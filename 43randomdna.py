b9ae646a87ef4ac596ed8ec523c734d611b5811e
Devin Fan <devinfan@MacBook-Air-359.local>
2024-02-09 15:41:54 -0800

    new

--- 43randomdna.py
+++ 43randomdna.py
@@ -1,9 +1,9 @@
-#Co-Authors - Devin Fan, Sophia Chen
+#Co-Authors - Devin Fan, Sophia Chen11
 
 import random
 
 for seq in range(3):
-	print("seq-", seq+1)
+	print(f">seq-{seq}")
 	for j in range(50):
 		print(random.choice('ACGT'), end='')
-	print("\n")
+	print()
