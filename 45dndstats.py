4cc8a99ed6e87dded4750b94e90ed65ade054362
Devin Fan <devinfan@MacBook-Air-359.local>
2024-02-09 16:02:23 -0800

    new

--- 45dndstats.py
+++ 45dndstats.py
@@ -9,16 +9,20 @@ for i in range(10000):
 		x = random.randint(1, 6)
 		total_3d6 += x
 
-print("3d6:", round(total_3d6 / 10000, 1))
+print("3d6:", round(total_3d6 / 10000, 4))
 
 total_3d6r1 = 0
 
 for i in range(10000):
 	for j in range(3):
-		x = random.randint(2, 6)
-		total_3d6r1 += x
+		x1 = random.randint(1, 6)
+		if x1 == 1:
+			x2 = random.randint(1, 6)
+			total_3d6r1 += x2
+		else: 
+			total_3d6r1 += x1
 
-print("3d6r1:", round(total_3d6r1 / 10000, 1))  
+print("3d6r1:", round(total_3d6r1 / 10000, 4))  
 
 total_3d6x2 = 0
 
@@ -31,7 +35,7 @@ for i in range(10000):
 		else:
 			total_3d6x2 += x1
             
-print("3d6x2:", round(total_3d6x2 / 10000, 1))
+print("3d6x2:", round(total_3d6x2 / 10000, 4))
 
 total_4D6d1 = 0
 
@@ -52,4 +56,4 @@ for i in range(10000):
             
 	total_4D6d1 += y1 + y2 + y3 + y4 - min_y
 
-print("4D6d1:", round(total_4D6d1 / 10000, 1))
+print("4D6d1:", round(total_4D6d1 / 10000, 4))
