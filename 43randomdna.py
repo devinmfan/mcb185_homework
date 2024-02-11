42239112147ffa0088c7d3eed42af48424192dca
devinmfan <dmfan@ucdavis.edu>
2024-02-10 21:54:37 -0800

    new

--- 43randomdna.py
+++ 43randomdna.py
@@ -4,6 +4,6 @@ import random
 
 for seq in range(3):
 	print(f">seq-{seq}")
-	for j in range(50):
+	for j in range(random.randint(50,60)):
 		print(random.choice('ACGT'), end='')
 	print()
--- 44randompi.py
+++ 44randompi.py
@@ -1,20 +1,14 @@
 #Co-Authors - Devin Fan, Sophia Chen
 
 import random
+import math
 
-inner = 0
-i = 1
-
+inside = 0
+total = 0
 while True:
 	x = random.random()
 	y = random.random()
-
-	origin = x**2 + y**2
-
-	if origin < 1:
-		inner += 1
-
-	pi_est = (inner / i) * 4
-
-	print(f"Iteration: {i + 1}, Pi Estimate: {pi_est}")
-	i += 1
+	d = math.sqrt(x**2 + y**2)
+	if d < 1: inside += 1
+	total += 1
+	print(4 * (inside / total))
