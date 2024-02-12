4fd0a1d43a6c91b6ae68875245d0a30b64cf4eb6
devinmfan <dmfan@ucdavis.edu>
2024-02-12 14:25:48 -0800

    new

---
+++ 51cdslength.py
@@ -0,0 +1,11 @@
+import gzip
+
+path = '../MCB185/data/GCF_000005845.2_ASM584v2_genomic.gff.gz'
+with gzip.open(path, 'rt') as fp:
+	for line in fp:
+		if line[0] != '#':
+			words = line.split()
+			if words[2] == 'CDS':
+				beg = int(words[3])
+				end = int(words[4])
+				print(end - beg + 1)
