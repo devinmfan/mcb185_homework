de50d66d279e8f8c7108e09ca29816685ade07ce
devinmfan <dmfan@ucdavis.edu>
2024-03-16 22:17:32 -0700

    new

--- 62skewer.py
+++ 62skewer.py
@@ -1,38 +1,37 @@
-import dogma
 import sys
 import mcb185
 
-seq = sys.argv[1]
+file = sys.argv[1]
 w = int(sys.argv[2])
 
-nts = []
-for defline, seq in mcb185.read_fasta(sys.argv[1]):
-	defwords = defline.split()
-	name = defwords[0]
-	for nt in seq:
-		nts.append(nt)
+for defline, seq in mcb185.read_fasta(file):
+    nts = []
+    for nt in seq:
+        nts.append(nt)
+    seq = ''.join(nts)
 
-seq = ''.join(nts)
+    g = seq[0:w].count('G')
+    c = seq[0:w].count('C')
 
-g = seq[0:w].count('G')
-c = seq[0:w].count('C')
-
-for i in range(len(seq) - w):
-	off = seq[i]
-	on = seq[i+w]
-	
-	if off == 'C': c -= 1
-	elif off == 'G': g -= 1
-	
-	if on == 'C': c += 1
-	elif on == 'G': g += 1
-	
-	gc = (c+g) / w
-	
-	if (g+c) > 0: 
-		skew = (g-c) / (g+c)
-	else: 
-		skew = 0
-	
-	print(i, gc, skew)
-
+    for i in range(len(seq) - w):
+        off = seq[i]
+        on = seq[i+w]
+        
+        if off == 'C':
+            c -= 1
+        elif off == 'G':
+            g -= 1
+        
+        if on == 'C':
+            c += 1
+        elif on == 'G':
+            g += 1
+        
+        gc = (c+g) / w
+        
+        if (g+c) > 0: 
+            skew = (g-c) / (g+c)
+        else: 
+            skew = 0
+        
+        print(i, gc, skew)
--- 63dust.py
+++ 63dust.py
@@ -7,40 +7,27 @@ file = sys.argv[1]
 window = int(sys.argv[2])
 e = float(sys.argv[3])
 
-nts = []
-
-for defline, seq in mcb185.read_fasta(sys.argv[1]):
+for defline, seq in mcb185.read_fasta(file):
 	defwords = defline.split()
 	name = defwords[0]
-	
 	defline_stuff = f">{name} {defline[len(name):]}"
-
 	
-	for nt in seq:
-		nts.append(nt)
-seq = ''.join(nts)
-
-result_nts = []
-
-for i in range(0, len(seq), window):
-	chunk = seq[i:i+window]
-	
-	count_a = chunk.count('A')
-	count_t = chunk.count('T')
-	count_g = chunk.count('G')
-	count_c = chunk.count('C')
-	
-	#print(chunk)
-	
-	h = dogma.entropy(count_a, count_c, count_t, count_g)
-	
-	for j in chunk:
-		if h < e:
-			chunk = chunk.replace(j, "N")
-
+	result_nts = []
+	for i in range(0, len(seq), window):
+		chunk = seq[i:i+window]
+		count_a = chunk.count('A')
+		count_t = chunk.count('T')
+		count_g = chunk.count('G')
+		count_c = chunk.count('C')
 		
-	result_nts.append(chunk)
+		h = dogma.entropy(count_a, count_c, count_t, count_g)
+		
+		for j in chunk:
+			if h < e:
+				chunk = chunk.replace(j, "N")
+        
+		result_nts.append(chunk)
 
-complete_genome = ''.join(result_nts)
-print(defline_stuff)
-print(complete_genome)
+	complete_genome = ''.join(result_nts)
+	print(defline_stuff)
+	print(complete_genome)
