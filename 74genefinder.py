1d74ce169683626e2c75f2061c6bf58ef754c530
devinmfan <dmfan@ucdavis.edu>
2024-03-20 21:21:08 -0700

    new

--- 74genefinder.py
+++ 74genefinder.py
@@ -15,17 +15,18 @@ for defline, seq in mcb185.read_fasta(sys.argv[1]):
 seq = ''.join(codon)
 anti_seq = mcb185.anti_seq(seq)
 
+stops_used = {} 
+
 for frame in range(3):
-	stops_used = {}
 	fseq = seq[frame:]
-    
+	
 	for i in range(0, len(seq) - 3 + 1, 3):
 		start_codon = fseq[i:i+3]
-        
+
 		if start_codon == 'ATG':
 			for j in range(i, len(fseq) - 2, 3):
 				stop_codon = fseq[j:j+3]
-                
+
 				if stop_codon == 'TAA' or stop_codon == 'TAG' or stop_codon == 'TGA':
 					stop = j
 					if stop not in stops_used:
@@ -35,16 +36,15 @@ for frame in range(3):
 						break
 
 for frame in range(3):
-	stops_used = {}
 	anti_fseq = anti_seq[frame:]
-    
+	
 	for i in range(0, len(anti_seq) - 3 + 1, 3):
 		start_codon = anti_fseq[i:i+3]
-        
+
 		if start_codon == 'ATG':
 			for j in range(i, len(anti_fseq) - 2, 3):
 				stop_codon = anti_fseq[j:j+3]
-			
+
 				if stop_codon == 'TAA' or stop_codon == 'TAG' or stop_codon == 'TGA':
 					stop = j
 					if stop not in stops_used:
