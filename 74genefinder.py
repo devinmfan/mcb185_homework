5f393529e2ad2091b95c78a951d4f34f54521eff
devinmfan <dmfan@ucdavis.edu>
2024-03-20 21:58:58 -0700

    new

--- 74genefinder.py
+++ 74genefinder.py
@@ -2,6 +2,7 @@ import sys
 import mcb185
 
 seq = sys.argv[1]
+gene_min = int(sys.argv[2])
 anti = mcb185.anti_seq(seq)
 
 codon = []
@@ -15,40 +16,44 @@ for defline, seq in mcb185.read_fasta(sys.argv[1]):
 seq = ''.join(codon)
 anti_seq = mcb185.anti_seq(seq)
 
-stops_used = {} 
+stops_used = {}
 
 for frame in range(3):
 	fseq = seq[frame:]
-	
+
 	for i in range(0, len(seq) - 3 + 1, 3):
-		start_codon = fseq[i:i+3]
+		start_codon = fseq[i:i + 3]
 
 		if start_codon == 'ATG':
 			for j in range(i, len(fseq) - 2, 3):
-				stop_codon = fseq[j:j+3]
+				stop_codon = fseq[j:j + 3]
 
 				if stop_codon == 'TAA' or stop_codon == 'TAG' or stop_codon == 'TGA':
 					stop = j
 					if stop not in stops_used:
-						print('gene:', i, j)
-						print('Found in the normal sequence')
-						stops_used[stop] = True
-						break
+						gene_length = stop - i + 3  
+						if gene_length >= gene_min:  
+							print('gene:', i, j)
+							print('Found in the normal sequence')
+							stops_used[stop] = True
+							break
 
 for frame in range(3):
 	anti_fseq = anti_seq[frame:]
-	
+
 	for i in range(0, len(anti_seq) - 3 + 1, 3):
-		start_codon = anti_fseq[i:i+3]
+		start_codon = anti_fseq[i:i + 3]
 
 		if start_codon == 'ATG':
 			for j in range(i, len(anti_fseq) - 2, 3):
-				stop_codon = anti_fseq[j:j+3]
+				stop_codon = anti_fseq[j:j + 3]
 
 				if stop_codon == 'TAA' or stop_codon == 'TAG' or stop_codon == 'TGA':
 					stop = j
 					if stop not in stops_used:
-						print('gene:', i, j)
-						print('Found in the anti-sequence')
-						stops_used[stop] = True
-						break
+						gene_length = stop - i + 3  
+						if gene_length >= gene_min:  
+							print('gene:', i, j)
+							print('Found in the anti-sequence')
+							stops_used[stop] = True
+							break
